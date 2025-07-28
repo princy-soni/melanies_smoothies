8019ab77cba9b086cb544d0d0174153f94569d22
princy-soni <92619417+princy-soni@users.noreply.github.com>
2025-07-28 21:50:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 # Title
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -41,6 +41,4 @@ if st.button('Submit Order'):
         # Optional: Show the SQL query (for debugging)
         # st.write(my_insert_stmt)
         st.stop()
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
