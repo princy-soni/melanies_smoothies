4de07e98a7394a22029ff419594f2c9609d76677
princy-soni <92619417+princy-soni@users.noreply.github.com>
2025-07-28 21:50:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 # Title
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
