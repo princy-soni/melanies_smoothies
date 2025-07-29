b0641f0f8fe6cd0516d3799d9c46bb23454d2355
princy-soni <92619417+princy-soni@users.noreply.github.com>
2025-07-29 20:38:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon=":white_check_mark:")
+        st.success('Your Smoothie is ordered!', icon="✅")
