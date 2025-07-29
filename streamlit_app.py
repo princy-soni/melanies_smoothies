ccc94384831062ea65761f23485ccee387c5c489
princy-soni <92619417+princy-soni@users.noreply.github.com>
2025-07-29 14:08:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ if st.button('Submit Order'):
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf.df = st.dataframe(data = smoothiefroot_response.json, use_container_width = True)
+sf.df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
